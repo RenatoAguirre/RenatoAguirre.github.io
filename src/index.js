ae5568eab1c969705de002c2b5b3e1ec1ffba765
RenatoAguirre <raaguirre1@miuandes.cl>
2024-03-11 19:33:11 -0300

    fix/fixed bug with text adder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const button = document.querySelector("#button");
 
 button.addEventListener("click", function() {
   const input = document.querySelector("#input");
-  const container = document.querySelector("#content");
+  const container = document.querySelector("#insert");
   const texto= document.createElement("p");
   texto.textContent = input.value;
   container.appendChild(texto)
