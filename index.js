bfa0b1284c409d2e48ecf0e6faae30c9f7a6197d
Rena <raaguirre1@miuandes.cl>
2024-03-11 13:14:50 -0300

    feat/first version of the app, all functonality but no styling yet

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+const button = document.querySelector("#button");
+
+button.addEventListener("click", function() {
+  const input = document.querySelector("#input");
+  const container = document.querySelector("#content");
+  const texto= document.createElement("p");
+  texto.textContent = input.value;
+  container.appendChild(texto)
+});
